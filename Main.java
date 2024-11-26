c5147297a934ce9acb7acdcc58075de272f8ab1b
Ubuntu <ubuntu@ip-172-31-34-207.eu-north-1.compute.internal>
2024-11-26 10:26:15 +0000

    Updated the print code.

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My Registration Number: 123456789");
+        System.out.println("My Registration Number: FA21-BCS-005");
     }
 }
